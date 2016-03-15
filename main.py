6a960b08a1e3c843cb845aaa6c43e530078c502d
yunbei <stevenchen20021@gmail.com>
2016-03-15 08:26:26 +0800

    Add the dependencies to mysql-tools of google.

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 # coding:utf8
 
+import argparse
+import os
+import subprocess
+import sys
+
+
+def ensure_dependencies():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    dependency_script = os.path.join(base_dir, "ensure_dependencies.py")
+
+    try:
+        subprocess.check_call([sys.executable, dependency_script, base_dir])
+    except subprocess.CalledProcessError as e:
+        print >>sys.stderr, e
+        print >>sys.stderr, "Failed to ensure dependencies being up-to-date!"
+
 
 def main():
-    pass
+    ensure_dependencies()
 
 
 if __name__ == '__main__':
